a1eca6ee1c008c871fab7db58ccfdf2409651efd
Flip Van Wijk <fvan-wij@f1r5s17.codam.nl>
2024-01-31 18:20:36 +0100

    Lil tweak

--- CPP_Module02/ex00/src/Fixed.cpp
+++ CPP_Module02/ex00/src/Fixed.cpp
@@ -1,17 +1,19 @@
 #include "Fixed.hpp"
 #include <iostream>
 
-Fixed::Fixed(){
+Fixed::Fixed() : _fpnVal(0) {
 	std::cout << "Default constructor called" << std::endl;
-	_fpnVal = 0;
 }
 
-Fixed::Fixed(const Fixed& other){
-	*this = other;
+Fixed::Fixed(const Fixed& other) : _fpnVal(other._fpnVal) {
 	std::cout << "Copy constructor called" << std::endl;
 }
 
-Fixed& Fixed::operator= (const Fixed& other){
+Fixed& Fixed::operator= (const Fixed& other) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this == &other) {
+		return (*this);
+	}
 	this->_fpnVal = other.getRawBits();
 	return (*this);
 }
