3a764d42d8ad0a3cff22e71328f03a106083f2f1
fvan-wij <fvan-wij@student.codam.nl>
2024-07-18 12:43:14 +0200

    Added error mssge

--- CPP_Module06/ex00/src/main.cpp
+++ CPP_Module06/ex00/src/main.cpp
@@ -3,8 +3,11 @@
 int main(int argc, char *argv[])
 {
 	if (argc == 2) {
-		// std::cout << 44. << std::endl;
-		return ScalarConverter::convert(argv[1]);
+		ScalarConverter::convert(argv[1]);
+		return 0;
 	}
-	return (0);
+	else {
+		std::cerr << "Provide strictly 1 argument to be converted. I.e: './app 42.0f'" << std::endl;
+		return 1;
+	}
 }
