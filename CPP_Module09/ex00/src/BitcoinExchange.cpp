54410fc88312043111a007f0cd0d430da20472d4
fvan-wij <fvan-wij@student.codam.nl>
2024-12-15 18:11:31 +0100

    Finished ex00

--- CPP_Module09/ex00/src/BitcoinExchange.cpp
+++ CPP_Module09/ex00/src/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 std::chrono::system_clock::time_point	BitcoinExchange::stringToDate(const std::string& dateStr)
 {
     std::tm tm = {};
-    std::istringstream ss(dateStr);
+    std::stringstream ss(dateStr);
     ss >> std::get_time(&tm, "%Y-%m-%d");
     
     if (ss.fail())
@@ -36,22 +36,22 @@ BitcoinExchange::BitcoinExchange(const std::filesystem::path& exchangeDataPath)
 	std::string line;
 	while(std::getline(file, line))
 	{
-		if (line != "date,exchange_rate")
+		if (line != "date,exchange_rate" && not line.empty())
 		{
 			std::string key = line.substr(0, line.find(","));
 			double value = std::stod(line.substr(line.find(",") + 1));
 			_priceMap.insert({stringToDate(key), value});
 		}
 	}
-	// printPriceMap();
 }
 
 void BitcoinExchange::printPriceMap()
 {
-	for (const auto& [date, value] : _priceMap) {
+	for (const auto& [date, value] : _priceMap)
+	{
 		std::time_t time = std::chrono::system_clock::to_time_t(date);
 		std::tm tm = *std::localtime(&time);
-		std::ostringstream date_stream;
+		std::stringstream date_stream;
 		date_stream << std::put_time(&tm, "%Y-%m-%d");
 		std::string date_str = date_stream.str();
 		std::cout << "Date: " << date_str << " -> Value: " << value << std::endl;
@@ -62,23 +62,21 @@ std::optional<double>	BitcoinExchange::getPriceAtDate(const std::string& date)
 {
 	auto tpDate = stringToDate(date);
 
-	if (_priceMap.find(tpDate) != _priceMap.end())
+	if (_priceMap.find(tpDate) != _priceMap.end()) // Is present
 	{
 		return _priceMap.at(tpDate);
 	}
 
-	if (tpDate < _priceMap.begin()->first)
+	if (tpDate < _priceMap.begin()->first) // Is past lowest entry
 	{
-		std::cout << "Under lower bounds" << std::endl;
 		return _priceMap.begin()->second;
 	}
-	else if (tpDate > _priceMap.rbegin()->first)
+	else if (tpDate > _priceMap.rbegin()->first) // Is past highest entry
 	{
-		std::cout << "Over upper bounds" << std::endl;
 		return _priceMap.rbegin()->second;
 	}
 
-	auto it = _priceMap.lower_bound(tpDate);
+	auto it = _priceMap.lower_bound(tpDate); // Nearest-lowest entry
 	if (it != _priceMap.end())
 	{
 		it--;
--- CPP_Module09/ex00/src/main.cpp
+++ CPP_Module09/ex00/src/main.cpp
@@ -1,8 +1,11 @@
 #include <fstream>
 #include <iostream>
 #include <filesystem>
+#include <climits>
 
 #include "BitcoinExchange.hpp"
+
+#define ARG_INPUT 1
 
 bool	validInput(std::filesystem::path inputPath)
 {
@@ -22,37 +25,94 @@ bool	validInput(std::filesystem::path inputPath)
 	return true;
 }
 
+bool	validDate(const std::string& date)
+{
+		std::stringstream ss(date);
+		int year, month, day;
+		char delimiter;
+		ss >> year >> delimiter >> month >> delimiter >> day;
+
+		if (std::to_string(year).length() != 4 || month > 12 || day > 31)
+			return false;
+		return true;
+}
+
+void	calcExchangeRatesFromFile(std::filesystem::path inputFile, BitcoinExchange& handler)
+{
+	std::string line;
+	float		value = 0.0f;
+	std::ifstream inFile(inputFile);
+	while (std::getline(inFile, line))
+	{
+		if (line.empty())
+		{
+			continue;
+		}
+
+		size_t	divider = line.find("|");
+		if (divider == std::string::npos)
+		{
+			std::cerr << "Error: bad input => " << line << std::endl;
+			continue;
+		}
+
+		std::string date 	= line.substr(0, line.find("|"));
+		if (date.empty())
+		{
+			std::cerr << "Error: bad input => " << line << std::endl;
+			continue;
+		}
+
+		if (not validDate(date))
+		{
+			std::cerr << "Error: bad date => " << line << std::endl;
+			continue;
+		}
+
+		try
+		{
+			value 	= std::stof(line.substr(line.find("|") + 1));
+			if (value < 0)
+			{
+				std::cerr << "Error: not a positive number." << std::endl;
+				continue;
+			}
+			if (value > 1000)
+			{
+				std::cerr << "Error: too large a number." << std::endl;
+				continue;
+			}
+			auto 		total 	= handler.getPriceAtDate(date).value_or(0) * value;
+			std::cout << date << " => " << value << " = " << total << std::endl;
+		}
+		catch (std::exception& e)
+		{
+			std::cerr << "Error: bad input => " << line << std::endl;
+			continue;
+		}
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		std::cerr << "Program requires input to run (e.g. ./btc input.txt)" << std::endl;
+		std::cerr << "Program requires valid input to run (e.g. ./btc input.txt)" << std::endl;
 		return -1;
 	}
 
-	std::filesystem::path inputPath(argv[1]);
+	std::filesystem::path inputFile(argv[ARG_INPUT]);
 
-	if (not validInput(inputPath))
+	if (not validInput(inputFile))
 	{
 		return -2;
 	}
 
 	try
 	{
-		std::filesystem::path exchange_path("data.csv");
-
-		BitcoinExchange handler(exchange_path);
-
-
-		std::string line;
-		std::ifstream inFile(inputPath);
-		while (std::getline(inFile, line))
-		{
-			std::string date 	= line.substr(0, line.find("|"));
-			float		value 	= std::stof(line.substr(line.find("|") + 1));
-			auto 		total 	= handler.getPriceAtDate(date).value_or(0) * value;
-			std:: cout << date << " => " << value << " = " << total << std::endl;
-		}
+		std::filesystem::path exchangePath("data.csv");
+		BitcoinExchange handler(exchangePath);
+		calcExchangeRatesFromFile(inputFile, handler);
 	}
 	catch (std::exception &e)
 	{
