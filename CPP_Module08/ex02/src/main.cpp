3bd3c5fabc283c93561be6aca730258f70826f1f
fvan-wij <vanwijkflip@gmail.com>
2025-02-21 15:25:22 +0100

    Added test

--- CPP_Module08/ex02/src/main.cpp
+++ CPP_Module08/ex02/src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <list>
 #include "MutantStack.hpp"
 
 void	subjectTest()
 {
-	std::cout << "--Subject test" << std::endl;
+	std::cout << "--Subject test with std::MutantStack" << std::endl;
 
 	MutantStack<int> mstack;
 	mstack.push(5);
@@ -19,7 +20,7 @@ void	subjectTest()
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
-	std::cout << "Testing iterators (old school)" << std::endl;
+	std::cout << "Testing iterators" << std::endl;
 	while (it != ite)
 	{
 		std::cout << *it << "\n";
@@ -35,7 +36,41 @@ void	subjectTest()
 	std::stack<int> s(mstack);
 }
 
+void	subjectTestWithList()
+{
+	std::cout << "--Subject test with std::list" << std::endl;
+
+	std::list<int> mstack;
+	mstack.push_back(5);
+	mstack.push_back(17);
+	std::cout << "Top: " << mstack.back() << "\n" << std::endl;
+	mstack.pop_back();
+	std::cout << "Size: " << mstack.size() << "\n" << std::endl;
+	mstack.push_back(3);
+	mstack.push_back(5);
+	mstack.push_back(737);
+	mstack.push_back(0);
+	std::list<int>::iterator it = mstack.begin();
+	std::list<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	std::cout << "Testing iterators" << std::endl;
+	while (it != ite)
+	{
+		std::cout << *it << "\n";
+		++it;
+	}
+	std::cout << std::endl;
+	std::cout << "Testing iterators range based loop" << std::endl;
+	for (auto &n : mstack)
+	{
+		std::cout << n << "\n";
+	}
+	std::cout << std::endl;
+}
+
 int main(void)
 {
 	subjectTest();
+	subjectTestWithList();
 }
